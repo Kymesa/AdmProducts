8bc3f407a0811344c58ebd7241ba6213d822fa65
Kymesa <Kymesa@unicesar.edu.co>
2023-08-24 12:51:35 -0500

    2.0.1

--- src/Hooks/useAxiosGet.jsx
+++ src/Hooks/useAxiosGet.jsx
@@ -4,12 +4,30 @@ import { useEffect, useState } from "react";
 function useAxiosGet(url) {
   const [data, setData] = useState(null);
   const [items, setItems] = useState(0);
+  const [error, setError] = useState(null);
+  const getData = async (params_url) => {
+    try {
+      const result = await axios(params_url);
+      setData(result.data.data);
+      setItems(result.data.totalItems);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   useEffect(() => {
-    axios.get(url).then((result) => setData(result.data.data));
-    axios.get(url).then((result) => setItems(result.data.totalItems));
+    getData(url);
+    // axios
+    //   .get(url)
+    //   .then((result) => setData(result.data.data))
+    //   .catch((e) => setError(e.message));
+    // axios
+    //   .get(url)
+    //   .then((result) => setItems(result.data.totalItems))
+    //   .catch((e) => setError(e.message));
   }, [url]);
 
-  return { data, items };
+  return { data, items, error };
 }
 
 export { useAxiosGet };
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,17 +2,19 @@ import TableHeader from "./TableHeader";
 import TableBody from "./TableBody";
 import Skeleton from "./Skeleton";
 import { useAxiosGet } from "../Hooks/useAxiosGet";
+const URL = "https://books-api-services.onrender.com/api/v1/books";
 
 function Table() {
-  const { data, items } = useAxiosGet(
-    "https://books-api-services.onrender.com/api/v1/books"
-  );
+  const { data, items, error, msj } = useAxiosGet(URL);
+
   return (
     <>
       <div className="flex flex-col">
         <div className="-m-1.5 overflow-x-auto">
           <div className="p-1.5 min-w-full inline-block align-middle">
             <div className="border rounded-lg overflow-hidden dark:border-gray-700">
+              {error && <strong>{error}</strong>}
+              {msj && <strong>SE HIZO LA PETICION</strong>}
               <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                 <TableHeader items={items} />
                 {data ? (
