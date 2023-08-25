20e462fa179d41a740c43a39c281910fef18f4a8
Kymesa <Kymesa@unicesar.edu.co>
2023-08-25 08:13:39 -0500

    6.0.0

--- src/components/ErrorApp.jsx
+++ src/components/ErrorApp.jsx
@@ -1,4 +1,4 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 export default function ErrorApp() {
   const error = useRouteError();
@@ -8,15 +8,20 @@ export default function ErrorApp() {
       <main>
         <div className="max-w-screen-xl mx-auto px-4 flex items-center justify-start h-screen md:px-8">
           <div className="max-w-lg mx-auto space-y-3 text-center">
-            <h3 className="text-red-500 font-semibold">{error.status} Error</h3>
+            <h3 className="text-red-500 font-semibold text-4xl">
+              {error.status} Error
+            </h3>
             <p className="text-white text-4xl font-semibold sm:text-5xl">
               {error.statusText}
             </p>
             <p className="text-white">{error.error["message"]}</p>
             <div className="flex flex-wrap items-center justify-center gap-3">
-              <a className="block py-2 px-4 text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg">
-                Go back
-              </a>
+              <Link to={"/"}>
+                <a className="block py-2 px-4 text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg">
+                  Go back
+                </a>
+              </Link>
+
               <a className="block py-2 px-4 text-white hover:bg-gray-50 font-medium duration-150 active:bg-gray-100 border rounded-lg">
                 Contact support
               </a>
